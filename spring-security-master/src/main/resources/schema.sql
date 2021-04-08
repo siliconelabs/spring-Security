c2994ccaabe358adeeecc27f18301f9067a812b4
siliconelabs <77060215+siliconelabs@users.noreply.github.com>
2021-04-09 00:55:26 +0300

    Add files via upload

---
+++ spring-security-master/src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS USERS (
+  userid INT PRIMARY KEY auto_increment,
+  username VARCHAR(20),
+  salt VARCHAR,
+  password VARCHAR,
+  firstname VARCHAR(20),
+  lastname VARCHAR(20)
+);
+
